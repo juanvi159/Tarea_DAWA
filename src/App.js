779c657ffaa8e43cb053345075cf5c08c43dc87b
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-03-29 17:38:03 -0500

    Componente imagen y resultado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import Buscador from './componentes/Buscador';
+import Resultado from './componentes/Resultado';
 
 class App extends Component{
 
   state={
-    termino : 'Cafe'
+    termino : '',
+    imagenes : []
   }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = `https://pixabay.com/api/?key=20919779-e5e89fad40d8fdf8113b1df2b&q=${termino}&per_page=30`;
+    //console.log(url);
+    fetch(url)
+      .then(respuesta => respuesta.json() )
+      .then(resultado => this.setState({ imagenes : resultado.hits}))
+  }
+
   datosBusqueda=(termino)=>{
     this.setState({
       termino
+    }, ()=>{
+      this.consultarApi();
     })
   }
   render() {
@@ -19,8 +33,11 @@ class App extends Component{
           <Buscador
             datosBusqueda={this.datosBusqueda}
           />
+          <Resultado
+          imagenes={this.state.imagenes}
+          />
         </div>
-        {this.state.termino}
+
       </div>
     );
   }
