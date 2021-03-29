632b788d81870ca8c850fea9f418dfb6d1ea4fe6
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-03-29 15:45:10 -0500

    Componente buscador

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Buscador from './componentes/Buscador';
 
 class App extends Component{
+
+  state={
+    termino : 'Cafe'
+  }
+  datosBusqueda=(termino)=>{
+    this.setState({
+      termino
+    })
+  }
   render() {
     return (
       <div className="app container">
         <div className="jumbotron">
           <p className="lead text-center">Buscador de Imagenes</p>
+          <Buscador
+            datosBusqueda={this.datosBusqueda}
+          />
         </div>
+        {this.state.termino}
       </div>
     );
   }
